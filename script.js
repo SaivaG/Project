8abf3389f11b3b0ff2763b94a704ee0a4e91f28f
SaivaG <saidahmetovaguzal@gmail.com>
2019-04-15 21:18:08 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var btn = document.querySelector('button'), // Главная кнопка
+    modal = document.querySelector('.modal'), // Модальное окно
+    closeBtn = document.querySelector('.closeBtn'); // Кнопка, закрывающая модальное окно
+
+// Открытие модального окна
+btn.addEventListener('click', function () {
+    modal.style.display = 'flex';
+})
+// Закрытие модального окна при клике на кнопку
+closeBtn.addEventListener('click', function () {
+    modal.style.display = "none";
+})
+// // Закрытие модального окна при клике на серую область
+// window.addEventListener('click', function (e) {
+//     if (e.target == modal) {
+//         modal.style.display = "none";
+//     }
+// })
+
+
+var a=0;
+forma.mytext.onkeypress=function(){
+    if(a==4){
+        a=0;
+        forma.mytext.value+=" ";
+    }
+    a++;
+}
